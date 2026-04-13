46b0c7cebf6d5efb715f6fe2c1e0100626379222
Samuel Stewart <ses1251@rit.edu>
2026-04-13 04:10:28 -0400

    Initial commit

---
+++ src/client/java/org/johnnymod/flashlightmod/client/FlashlightmodClient.java
@@ -0,0 +1,10 @@
+package org.johnnymod.flashlightmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FlashlightmodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/johnnymod/flashlightmod/FlashlightColor.java
@@ -0,0 +1,4 @@
+package org.johnnymod.flashlightmod;
+
+public class FlashlightColor {
+}
